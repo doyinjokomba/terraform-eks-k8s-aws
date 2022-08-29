9dbc8608262519d038d2cb97005a0d964d4ef489
doyinjokomba <doyinj2000@yahoo.com>
2022-08-29 17:51:14 +0100

    Add files via upload

--- vpc/main.tf
+++ vpc/main.tf
@@ -12,7 +12,6 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.14.2"
 
-
   name                 = var.vpc_name
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
